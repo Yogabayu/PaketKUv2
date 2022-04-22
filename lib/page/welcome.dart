857f105d70b139f2aa405b9141739acb8639fe56
Yoga Bayu AP <yogabayusbi@gmail.com>
2022-04-22 22:53:57 +0700

    fix dashboard

--- lib/page/welcome.dart
+++ lib/page/welcome.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tracking/controller/theme_controller.dart';
@@ -27,24 +28,31 @@ class _WelcomeState extends State<Welcome> {
             begin: Alignment.topRight,
             end: Alignment.bottomLeft,
             colors: [
-              Color.fromRGBO(255, 222, 102, 100),
+              Color.fromARGB(156, 245, 196, 20),
               Color.fromRGBO(255, 237, 34, 100),
             ],
           )),
           child: ListView(
             children: [
               Container(
+                decoration: new BoxDecoration(
+                  borderRadius: new BorderRadius.only(
+                    bottomLeft: Radius.circular(50),
+                    bottomRight: Radius.circular(50),
+                  ),
+                  color: Color.fromARGB(255, 244, 233, 179),
+                ),
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(20.0), //or 15.0
                   child: Container(
-                    height: height / 3,
+                    height: height / 2,
                     width: width,
-                    child: Image.asset("assets/image/astronout.png"),
+                    child: Image.asset("assets/image/final.png"),
                   ),
                 ),
               ),
               SizedBox(
-                height: height * 0.2,
+                height: height * 0.06,
               ),
               Center(
                 child: Text(
@@ -66,6 +74,73 @@ class _WelcomeState extends State<Welcome> {
                     color: Colors.black.withOpacity(0.5),
                     fontSize: height * 0.02,
                     fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: height * 0.08,
+              ),
+              InkWell(
+                child: Container(
+                    width: width / 2,
+                    height: height / 18,
+                    margin:
+                        EdgeInsets.only(left: width * 0.2, right: width * 0.2),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.black),
+                    child: Center(
+                        child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Container(
+                          height: 30.0,
+                          width: 30.0,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image:
+                                    AssetImage('assets/image/google_logo.png'),
+                                fit: BoxFit.cover),
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                        Text(
+                          'Sign in with Google',
+                          style: TextStyle(
+                              fontSize: height * 0.02,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                      ],
+                    ))),
+                onTap: () => {
+                  print("object"),
+                },
+              ),
+              SizedBox(
+                height: height * 0.03,
+              ),
+              Center(
+                child: RichText(
+                  text: TextSpan(
+                    text: 'Don\'t have any account ? ',
+                    style: TextStyle(
+                      fontSize: height * 0.015,
+                      color: Colors.black.withOpacity(0.5),
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: 'Sign Up',
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              print('The button is clicked!');
+                            },
+                          style: TextStyle(
+                            fontSize: height * 0.015,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 0, 0, 0),
+                          )),
+                    ],
                   ),
                 ),
               ),
